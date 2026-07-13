b77ca2f1f54eea3360272202bd0c3a443b0a709e
Ali Sajjad <alisajjad.webxity@gmail.com>
2026-07-14 03:43:59 +0500

    updated about section

---
+++ src/constants/index.js
@@ -0,0 +1,160 @@
+import {
+  wordpress,
+  html,
+  css,
+  tailwind,
+  javascript,
+  js,
+  reactjs,
+  nodejs,
+  php,
+  python,
+  git,
+  edunet,
+  weatherpedia,
+  termpw,
+  payloadmaster,
+  threejs,
+  mhft,
+  sketcher,
+  mathwork,
+  CompileVortex,
+  eduskill,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  { title: "WordPress", icon: wordpress },
+  { title: "JavaScript", icon: js },
+  { title: "PHP", icon: php },
+  { title: "Python", icon: python },
+];
+
+export const technologies = [
+  { name: "HTML 5", icon: html },
+  { name: "CSS 3", icon: css },
+  { name: "JavaScript", icon: javascript },
+  { name: "Rect JS", icon: reactjs },
+  { name: "Tailwind CSS", icon: tailwind },
+  { name: "Node JS", icon: nodejs },
+  { name: "Three JS", icon: threejs },
+  { name: "git", icon: git },
+];
+
+export const experiences = [
+  {
+    title: "Plugin Developer",
+    company_name: "WooNinjas",
+    icon: eduskill,
+    iconBg: "#161329",
+    date: "Sep 2023 - Continue",
+    points: [
+      "Successfully delivered large-scale custom development projects from concept to deployment, ensuring reliable solutions.",
+      "Work under pressure to meet hard deadlines, managing complex requirements and ensuring timely, high-quality execution.",
+      "Working with a team to create custom plugins that meet client's requirements and exceed expectations.",
+    ],
+  },
+  {
+    title: "WordPress Developer",
+    company_name: "eTekStudio",
+    icon: mathwork,
+    iconBg: "#161329",
+    date: "Apr 2023 - Sep 2023",
+    points: [
+      "Provide fast and secure websites to clients along with technical support.",
+      "Optimized website performance, troubleshot and resolved complex technical issues.",
+    ],
+  },
+  {
+    title: "WordPress Developer",
+    company_name: "Regal Systems",
+    icon: edunet,
+    iconBg: "#161329",
+    date: "Jun 2018 - Mar 2023",
+    points: ["Work with clients to understand their requirements and provide solutions."],
+  },
+];
+
+export const projects = [
+  {
+    name: "WeatherPedia",
+    description:
+      "Web-based platform that allows users to access weather information for their location by entering it in the search field",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "HTML", color: "green-text-gradient" },
+      { name: "bootstrap 5.3.0", color: "pink-text-gradient" },
+      { name: "Weather API by API Ninjas", color: "yellow-text-gradient" },
+    ],
+    image: weatherpedia,
+    source_code_link: "https://github.com/lohitkolluri/WeatherPedia",
+  },
+  {
+    name: "Terminal Like Portfolio Website",
+    description:
+      "A terminal themed portfolio website that allows users to type into the terminal and use commands like a real terminal.",
+    tags: [
+      { name: "HTML", color: "blue-text-gradient" },
+      { name: "css", color: "green-text-gradient" },
+      { name: "Javascript", color: "pink-text-gradient" },
+    ],
+    image: termpw,
+    source_code_link: "https://github.com/lohitkolluri/lohitkolluri.github.io",
+  },
+  {
+    name: "Mental Health Fitness Tracker",
+    description:
+      "ML model that utilizes regression techniques to provide insights into mental health and make predictions based on the available data.",
+    tags: [
+      { name: "Machine Learning", color: "blue-text-gradient" },
+      { name: "Jupyter Notebook", color: "green-text-gradient" },
+      { name: "Regression Algorithms", color: "pink-text-gradient" },
+    ],
+    image: mhft,
+    source_code_link: "https://github.com/lohitkolluri/mental_health_fitness_tracker",
+  },
+  {
+    name: "PayloadMaster",
+    description: "Tool to automate payload creation using the Metasploit framework",
+    tags: [{ name: "shell", color: "blue-text-gradient" }],
+    image: payloadmaster,
+    source_code_link: "https://github.com/lohitkolluri/PayloadMaster",
+  },
+  {
+    name: "CompileVortex",
+    description: "Tool to automate payload creation using the Metasploit framework",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "CSS", color: "green-text-gradient" },
+      { name: "HTML", color: "pink-text-gradient" },
+    ],
+    image: CompileVortex,
+    source_code_link: "https://github.com/lohitkolluri/CompileVortex",
+  },
+  {
+    name: "Sketcher",
+    description: "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
+    tags: [
+      { name: "OpenCV", color: "blue-text-gradient" },
+      { name: "Matplotlib", color: "green-text-gradient" },
+      { name: "Python", color: "pink-text-gradient" },
+    ],
+
+    image: sketcher,
+    source_code_link: "https://github.com/lohitkolluri/Image_to_Pencil_Sketch_App",
+  },
+];
